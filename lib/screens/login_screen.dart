81b94dbc3a477bff420d353eb11112fcffeae2e7
Jonathan Josafat Contreras Villanueva <contreras_villanueva@live.com.mx>
2023-08-29 17:52:38 -0600

    Modifique el login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,6 +10,35 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TextEditingController txtConUser = TextEditingController();
+    final txtUser = TextField(
+      controller: txtConUser,
+    );
+
+    TextEditingController txtConPass = TextEditingController();
+    final txtPass = SizedBox(
+      width: 200,
+      height: 200,
+      child: Center(
+        child: TextField(
+          controller: txtConPass,
+        ),
+      ),
+    );
+
+    final imgFondo = Opacity(
+      opacity: .4,
+      child: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://todoimagenesde.com/wp-content/uploads/2018/10/FondosPantalla41.jpg')))),
+    );
+
+    return Scaffold(
+      body: Stack(
+        children: [imgFondo, txtUser, txtPass],
+      ),
+    );
   }
 }
